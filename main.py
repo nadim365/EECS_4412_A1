058de0f3649394075091bea80f4aeaf3352f4b61
Nadim <samnad14@gmail.com>
2023-10-05 11:44:13 -0400

    added some print lines for debugging

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ CONFIDENCE_THRESHOLD = 0.6
 
 def main():
     file_handler = FileHandlingTools('walmart1.csv', 'ID2Name.csv')
-    print(f'HERE1')
+    print(f'LOADING TRANSACTIONS....')
     transactions = file_handler.load_transactions()
     frequent_itemsets_calc = FrequentItemsetCalculator1(transactions, math.ceil(SUPPORT_THRESHOLD * len(transactions)))
     prev_level = frequent_itemsets_calc.count_1_itemsets()
-    print(f'HERE11')
+    print(f'GENERATING ITEMSETS.....')
     k=2
     while prev_level:
         C_k = frequent_itemsets_calc.generate_candidate(k)
@@ -22,15 +22,15 @@ def main():
         prev_level = L_k
         k += 1
     print(frequent_itemsets_calc.levels_itemset)
-    print(f'HERE111')
+    print(f'GENERATING RULES.....')
     rule_generator = RuleGenerator1(frequent_itemsets_calc.levels_itemset, len(transactions))
     rules = rule_generator.generate_rules(CONFIDENCE_THRESHOLD)
     rules = rule_generator.quality_prune(rules)
-    print(f'HERE1111')
+    print(f'FORMATTING AND PRINTING THE ITEMSETS AT EACH LEVEL......')
     print("{} Levels for frequent itemsets".format(len(frequent_itemsets_calc.levels_itemset)))
     for level, itemsets in frequent_itemsets_calc.levels_itemset.items():
         print("level {}: {}".format(level, len(itemsets)))
-    print(f'HERE11111')
+    print(f'FORMATTING AND PRINTING THE RULES GENERATED.......')
     print("------------------------------------")
     print("{} Rules have been generated".format(len(rules)))
     for rule in rules:
